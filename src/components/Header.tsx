7af3bc61ffd7399f04f9e2645e101d59fad4c124
Salonii2004 <145736735+Salonii2004@users.noreply.github.com>
2026-03-19 16:55:08 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="text-2xl font-bold text-white">
-          <span className="text-red-500">Saloni</span>Flix
+          <span className="text-red-500">Saloni</span>Stack
         </Link>
 
         {/* Desktop Navigation */}
